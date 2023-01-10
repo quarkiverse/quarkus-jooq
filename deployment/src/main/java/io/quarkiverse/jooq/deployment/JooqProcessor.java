a60bffb1ad00311831a501635ad7590a25426b1a
Sadi Nasib <sadi.nasib@outlook.com>
2023-01-10 22:14:57 +0400

    upgrade to quarkus 3 and jakarta

--- deployment/src/main/java/io/quarkiverse/jooq/deployment/JooqProcessor.java
+++ deployment/src/main/java/io/quarkiverse/jooq/deployment/JooqProcessor.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Optional;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Default;
-import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
-import javax.inject.Singleton;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Default;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
 import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationValue;
